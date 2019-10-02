83fa70789789cbc8272fb740d4b6c588f9ae4030
Corpse Le <Corpse Le>
2019-10-02 20:37:19 +0800

    first commit

---
+++ src/main/java/WebTest/WebTest/CreateUserServlet.java
@@ -0,0 +1,48 @@
+package WebTest.WebTest;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class CreateUserServlet
+ */
+@WebServlet(
+		urlPatterns = { "/CreateUserServlet" }, 
+		initParams = { 
+				@WebInitParam(name = "username", value = ""), 
+				@WebInitParam(name = "password", value = ""), 
+				@WebInitParam(name = "date", value = "")
+		})
+public class CreateUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public CreateUserServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendRedirect("/index.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
